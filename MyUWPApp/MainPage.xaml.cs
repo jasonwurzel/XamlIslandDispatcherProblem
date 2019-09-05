465f6782b095d797b569f90a8a8066e20c75fdad
Michael Kreich <michaelkreich@hotmail.com>
2019-09-05 10:48:31 +0200

    replaced the whole solution with cleaner solution as proposed in https://github.com/windows-toolkit/Microsoft.Toolkit.Win32/issues/159#issuecomment-517031971

---
+++ MyUWPApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+using ReactiveUI;
+
+namespace MyUWPApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += (_, __) =>
+            {
+                var mainThreadScheduler = RxApp.MainThreadScheduler;
+            };
+        }
+    }
+}
